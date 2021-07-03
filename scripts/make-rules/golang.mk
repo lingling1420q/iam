310fb877e5962a133cdea238dd7045468250c145
Lingfei Kong <lkong@tencent.com>
2021-07-03 08:03:40 +0800

    feat: 1111

--- scripts/make-rules/golang.mk
+++ scripts/make-rules/golang.mk
@@ -75,6 +75,7 @@ go.lint: tools.verify.golangci-lint
 	@echo "===========> Run golangci to lint source codes"
 	@echo "glangci-lint version: $(shell golangci-lint version)"
 	@echo "1111111111111111: $(shell golangci-lint linters)"
+	@golangci-lint cache clean
 	@golangci-lint run -c $(ROOT_DIR)/.golangci.yaml $(ROOT_DIR)/...
 
 .PHONY: go.test
